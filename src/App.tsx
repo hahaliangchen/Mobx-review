4057beb9ab5bcde269681da508b4dee84b9034ee
chenge_cangku <7362213+chenge_cangku@user.noreply.gitee.com>
2022-04-10 20:10:42 +0800

    去除额外代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import './App.css';
 import {Button} from 'antd'
 import { testMobx } from './testMobx';
-import AliplayVideo from './aliplayer';
+
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Button type='primary' onClick={()=>{testMobx.increment()}}>哈哈</Button>
-      <AliplayVideo/>
+     
     </div>
   );
 }
